bf2de801e87b9b1c976745d40c6af684425de388
Kseniya St <kseniya7991@gmail.com>
2022-03-09 22:23:00 +0300

    disable scroll on popup

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,18 +1,24 @@
 const popup = document.querySelector('.popup'),
     popupStatus = popup.querySelector('.popup__status'),
     popupStatusDescription = popup.querySelector('.popup__description'),
-    popupCloseButtons = popup.querySelectorAll('.close-btn');
+    popupCloseButtons = popup.querySelectorAll('.close-btn'),
+    scroll = calcScroll();
 
     //Открытие попапа (после отправки формы)
 export const openPopup = (status, description) => {
     popupStatus.textContent = status;
     popupStatusDescription.textContent = description;
     popup.classList.add('popup_active');
+    document.body.style.overflow = "hidden";
+    document.body.style.marginRight = `${scroll}px`;
+    
 }
 
     //Закрытие попапа (при клике на оверлей или на одну из кнопок для закрытия)
 const closePopup = () => {
     popup.classList.remove('popup_active');
+    document.body.style.overflow = "scroll";
+    document.body.style.marginRight = `0px`;
 }
 
 
@@ -29,6 +35,24 @@ export const handlePopup = () => {
             closePopup();
         })
     })
+
+    calcScroll();
 }
+
+function calcScroll() {
+    let div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    console.log(div.offsetWidth, div.clientWidth)
+    div.remove();
+
+    return scrollWidth;
+  }
 
 
